1d96f133c12312cd440fe4a56225f612eb56a1bd
김강진 <@10.16.155.42>
2019-07-16 21:29:43 +0900

    Beginner's 낙서장

---
+++ src/Beginner1.java
@@ -0,0 +1,54 @@
+
+public class Beginner1 {
+
+	public static void main(String[] args) 
+	{	String slang="fuckyou";
+		System.out.println(slang.substring(0,2));
+		
+		// Main Method
+		} 
+	
+	
+	
+		
+		}
+	
+	
+	
+	
+			
+		
+		
+		
+
+
+
+
+
+	
+
+
+
+
+	
+	
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+	
+
+
+
+
+
+
+		
+		
+		
