e1a4f0530a7b4f009fb334339e8248743bc4f1fd
김강진 <@10.16.155.42>
2019-07-19 12:16:42 +0900

    Method 종류

---
+++ src/MethodStudying.java
@@ -0,0 +1,24 @@
+
+public class MethodStudying {
+	// public 리턴타입 메소드명 (매개변수) { 구현 할것}
+	
+		
+	public void method1 () {
+		// void는 return type이 없는 경우!!
+	} 
+
+	public void mehtod2(int k) {
+	
+	}
+	public int method3() { 
+		System.out.println("m3실행");
+		return 10;
+	}
+	
+	public void method4(int x, int y) {
+		System.out.println("m4실행");
+	}
+	
+	
+}
+ 
