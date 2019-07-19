5a0d0fb0dada569b8c76dc2005de7764852b0422
김강진 <@10.16.155.42>
2019-07-19 12:19:32 +0900

    // 5가지 method 선언방식

--- src/MethodStudying.java
+++ src/MethodStudying.java
@@ -2,7 +2,7 @@
 public class MethodStudying {
 	// public 리턴타입 메소드명 (매개변수) { 구현 할것}
 	
-		
+	//5가지 형태의 Method 선언 
 	public void method1 () {
 		// void는 return type이 없는 경우!!
 	} 
@@ -16,9 +16,13 @@ public class MethodStudying {
 	}
 	
 	public void method4(int x, int y) {
-		System.out.println("m4실행");
+		System.out.println(x+y+"m4");
 	}
 	
+	public int method5 (int y) {
+		System.out.println(y+"m5");
+		return y*2;
+	}
 	
 }
  
