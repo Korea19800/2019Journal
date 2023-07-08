bbc38ea51910cb798b7d04b4516f2fed98a99544
FrugalBoy <korea19800@gmail.com>
2023-07-09 02:34:25 +0900

    Update Beginner1.java

--- src/Beginner1.java
+++ src/Beginner1.java
@@ -1,54 +1,9 @@
 
 public class Beginner1 {
-
-	public static void main(String[] args) 
-	{	String slang="fuckyou";
+	public static void main(String[] args) {	
+		String slang="fuckyou";
 		System.out.println(slang.substring(0,2));
-		
 		// Main Method
-		} 
+	} 	
+}
 	
-	
-	
-		
-		}
-	
-	
-	
-	
-			
-		
-		
-		
-
-
-
-
-
-	
-
-
-
-
-	
-	
-	
-	
-	
-		
-		
-		
-		
-		
-		
-		
-	
-
-
-
-
-
-
-		
-		
-		
